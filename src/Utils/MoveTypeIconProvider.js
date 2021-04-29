3901879ab1c98d23b76b2f6e426f0e0cef55b13d
kecerud <sdurecek@gmail.com>
2021-04-29 22:32:31 +0200

    update git ignorecase false

---
+++ src/Utils/MoveTypeIconProvider.js
@@ -0,0 +1,10 @@
+import physicalIcon from '../Icons/MoveTypeIcons/move-physical.png';
+import specialIcon from '../Icons/MoveTypeIcons/move-special.png';
+import statusIcon from '../Icons/MoveTypeIcons/move-status.png';
+
+const MOVE_ICONS = {
+  physical: physicalIcon,
+  special: specialIcon,
+  status: statusIcon,
+};
+export default MOVE_ICONS;
